abeec3f374fe1312cafd15dc5549bcf429ae2af3
papingothegamer <toluwanimidaramola8@gmail.com>
2024-03-01 22:18:15 +0100

    project hint

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -5,7 +5,7 @@ const Bio: React.FC = () => {
   return (
     <div className={styles.bio}>
       <p className={styles.bioText}>
-      Hi. I'm Mariam, and I'm a passionate designer with a knack for creating awesome user experiences. With expertise in UI/UX, product, and content design, I love crafting seamless and captivating designs. I have over three years of experience in the design and media industry, and I'm all about creatively solving problems and driving innovation while keeping things fun and user-friendly. Let's create something amazing together!
+      Hi. I'm Mariam, and I'm a passionate designer with a knack for creating awesome user experiences. With expertise in UI/UX, product, and content design, I love crafting seamless and captivating designs. I have over three years of experience in the design and media industry, and I'm all about creatively solving problems and driving innovation while keeping things fun and user-friendly. Projects I have worked on and am currently working on are linked below for your viewing. Let's create something amazing together!
       </p>
     </div>
   );
